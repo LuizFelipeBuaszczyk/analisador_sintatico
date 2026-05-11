eb90eaff08aada2d9df4df330da33cbfd33717bb
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2026-05-11 20:27:32 -0300

    feat: Funcionamento completo do analisador sintático

--- src/AnalisadorSintatico.js
+++ src/AnalisadorSintatico.js
@@ -1,3 +1,64 @@
 
 
+export class AnalisadorSintatico {
+    
+    constructor(){
+    this.parseTable = {
+        'S': {
+            'a': 'aBb',
+            'b': 'bAc',
+            'c': 'cCb'
+        },
+        'A': {
+            'a': 'aCb',
+            'b': 'epsilon',
+            'c': 'epsilon'
+        },
+        'B': {
+            'a': 'aCa',
+            'b': 'bAb'
+        },
+        'C': {
+            'a': 'aB',
+            'c': 'cAc'
+        }
+    };
 
+    this.stack = ['$', 'S'];
+    }
+    
+    // Analise Sintática completa
+    resolve(sentence) {
+        sentence = sentence + '$';
+        while(sentence.length > 0){
+            const terminal = this.stack.pop();
+            const letter = sentence[0];
+           
+            if (terminal == '$' && letter == '$') {
+                return true;
+            }
+            if (letter == '$') return false;
+            
+            // Verificar se é um terminal
+            if (terminal.charCodeAt(0) >= 97 && terminal.charCodeAt(0)<=122){
+                if (letter == terminal) {
+                    sentence = sentence.slice(1, sentence.length);
+                    continue;
+                }
+                return false;
+            }
+            
+            // Não terminal
+            const action = this.parseTable[terminal][letter];
+            if (!action) {
+                return false;
+            }
+            
+            if (action == 'epsilon') continue;
+
+            for (let i=action.length-1; i>=0; i--){
+                this.stack.push(action[i]);
+            }
+        }
+    }
+}
--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
 // Interage com a Página
+import { AnalisadorSintatico } from './AnalisadorSintatico.js'
+
+
+document
+    .getElementById("btAnalisar")
+    .addEventListener("click", btAnalisar)
+
+
+
+
+function btAnalisar(){
+    const sentence = document.getElementById("inputSentence").value;
+    
+    const analisadorSintatico = new AnalisadorSintatico();
+
+    const result = analisadorSintatico.resolve(sentence);
+
+    console.log(result);
+}
+
