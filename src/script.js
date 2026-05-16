f34db5c7e6ff6e8f1dfa94bbda8f763da22b355f
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2026-05-16 14:28:20 -0300

    feat: Tabela de Parsing para gerar a sentença

--- src/script.js
+++ src/script.js
@@ -10,6 +10,22 @@ document
     .getElementById("btNextStep")
     .addEventListener("click", btNextStep);
 
+document
+    .getElementById("btGenerateSentence")
+    .addEventListener("click", openModal);
+
+document
+    .getElementById("btExitModal")
+    .addEventListener("click", exitModal);
+
+const actionCellElements = document.getElementsByClassName("actionCell")
+
+for (const element of actionCellElements) {
+    element.addEventListener("click", (event) => { 
+        getCellAction(event);
+    });
+}
+
 let operations = [];
 let step = undefined;
 
@@ -40,4 +56,18 @@ function btNextStep() {
     cellAction.innerHTML = operations[step]['action'];
     
     step++;
+}
+
+function openModal() {
+    const modal = document.getElementById('modalGenerateSentence');
+    modal.showModal();
+}
+
+function exitModal() {
+    const modal = document.getElementById('modalGenerateSentence');
+    modal.close();
+}
+
+function getCellAction(e) {
+    console.log(e);
 }
